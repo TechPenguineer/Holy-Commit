d7c223fe2bff01cc20f8ddf7ea215f58ba322b90
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-10-04 16:51:43 -0400

    Update commit.py

--- commit.py
+++ commit.py
@@ -1,12 +1,20 @@
 import os
 
 ip = 200000
-
+current = 1
+targed_to_push = 100
 autoPush = "n"
 
-for i in range(ip):
+
+while(ip != -1):
+	current = current+1
+
+	if(current==targed_to_push):
+		os.system("git push")
+		targed_to_push = current+targed_to_push
+		print("Pushed")
 	# os.system('git commit --allow-empty -m "New Commit at: $(date)"')
-	os.system("git commit --allow-empty -m 'Commit"+ str(i)+"'")	
+	os.system("git commit --allow-empty -m 'Commit"+ str(current)+"'")	
 
 print("Commited " + str(ip) + " times")
 
