70248e0378d2a6d1f3469005d7ac872dd0cf6e26
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-10-03 23:10:53 -0400

    Create commit.py

---
+++ commit.py
@@ -0,0 +1,14 @@
+import os
+
+ip = int(input("How many times do you want to commit? \n"))
+
+autoPush = input("Auto git push when commited? (y/n) \n")
+
+for i in range(ip):
+	# os.system('git commit --allow-empty -m "New Commit at: $(date)"')
+	os.system("git commit --allow-empty -m 'Commit"+ str(i) +" of " + str(ip) + "'")	
+
+print("Commited " + str(ip) + " times")
+
+if autoPush == "y":
+	os.system('git push')
