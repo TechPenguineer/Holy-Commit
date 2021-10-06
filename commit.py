cbede71b295461578eba0d0f0fe88d4876a9a797
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-10-06 17:43:44 -0400

    Update commit.py

--- commit.py
+++ commit.py
@@ -2,7 +2,7 @@ import os
 
 ip = 200000
 current = 1
-targed_to_push = 100
+targed_to_push = 1000
 autoPush = "n"
 
 
@@ -11,12 +11,12 @@ while(ip != -1):
 
 	if(current==targed_to_push):
 		os.system("git push")
-		targed_to_push = current+targed_to_push
+		targed_to_push = current+1000
 		print("Pushed")
 	# os.system('git commit --allow-empty -m "New Commit at: $(date)"')
 	os.system("git commit --allow-empty -m 'Commit"+ str(current)+"'")	
+	print("Commited " + str(current) + " times\nCurrent Push Target is: " + str(targed_to_push) + "\n\n")
 
-print("Commited " + str(ip) + " times")
 
 if autoPush == "y":
 	os.system('git push')
